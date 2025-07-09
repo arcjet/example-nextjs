7698d5bb8c414a84ca8011bcb3bab86911b7bdf1
Quinn Blenkinsop <quinn@arcjet.com>
2025-07-09 21:56:08 +0000

    chore: fmt

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -4,7 +4,8 @@ export function SignIn() {
   if (process.env.AUTH_GITHUB_ID === undefined) {
     return (
       <p>
-        Configure your GitHub OAuth app credentials in <code>.env</code> to enable sign in.
+        Configure your GitHub OAuth app credentials in <code>.env</code> to
+        enable sign in.
       </p>
     );
   }
@@ -17,12 +18,10 @@ export function SignIn() {
       }}
       className="form"
     >
-      <p>
-        Want to try a different rate limit?
-      </p>
+      <p>Want to try a different rate limit?</p>
       <button type="submit" className="button-secondary">
-          Sign in with GitHub
-        </button>
+        Sign in with GitHub
+      </button>
     </form>
   );
 }
